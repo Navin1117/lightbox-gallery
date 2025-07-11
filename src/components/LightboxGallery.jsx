61d8ecffd3c1ec125b7d0961aca76b83327c3124
Navin1117 <naveendchouhan1117@gmail.com>
2025-07-11 06:22:18 +0530

    Added new image to gallery

--- src/components/LightboxGallery.jsx
+++ src/components/LightboxGallery.jsx
@@ -3,7 +3,7 @@ import "../style.css";
 
 const images = [
   require("../images/img1.jpg"),
-  require("../images/img2.jpg"),
+  require("../images/img2new.jpeg"),
   require("../images/img3.jpg"),
   require("../images/img4.jpg"),
   require("../images/img5.jpg"),
